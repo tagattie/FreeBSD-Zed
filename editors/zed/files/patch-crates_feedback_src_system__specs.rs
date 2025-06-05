10e6c1c7632fd336862ed4b0d3b7428413bb0f52
Hiroki Tagato <tagattie@gmail.com>
2025-06-05 14:37:36 +0900

    Update to 0.189.5
    Changelog:
    - https://github.com/zed-industries/zed/releases/tag/v0.188.6
    - https://github.com/zed-industries/zed/releases/tag/v0.189.5

--- editors/zed/files/patch-crates_feedback_src_system__specs.rs
+++ editors/zed/files/patch-crates_feedback_src_system__specs.rs
@@ -1,6 +1,6 @@
---- crates/feedback/src/system_specs.rs.orig	2025-05-14 14:15:56 UTC
+--- crates/feedback/src/system_specs.rs.orig	2025-06-04 14:12:33 UTC
 +++ crates/feedback/src/system_specs.rs
-@@ -125,7 +125,7 @@ fn try_determine_available_gpus() -> Option<String> {
+@@ -123,7 +123,7 @@ fn try_determine_available_gpus() -> Option<String> {
  }
  
  fn try_determine_available_gpus() -> Option<String> {
@@ -9,7 +9,7 @@
      {
          return std::process::Command::new("vulkaninfo")
              .args(&["--summary"])
-@@ -144,7 +144,7 @@ fn try_determine_available_gpus() -> Option<String> {
+@@ -142,7 +142,7 @@ fn try_determine_available_gpus() -> Option<String> {
              })
              .or(Some("Failed to run `vulkaninfo --summary`".to_string()));
      }
